104f363cec42d44804609838e21cf46bce848a38
Yash Shah <yashshah00@outlook.com>
2025-07-30 17:58:46 +0100

    Updated .gitignore to avoid data directory, and reorganized storage of data.

--- download_files.sh
+++ download_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BOWLS=./bowls/
+BOWLS=./data/bowls/
 mkdir -p "$BOWLS"
 curl https://www.csun.edu/science/ref/games/questions/97_phys.pdf > "$BOWLS"/phys.pdf
 curl https://www.csun.edu/science/ref/games/questions/97_biol.pdf > "$BOWLS"/biol.pdf
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from scrape_sources import scrape_bowl
 
 
 def main():
-    scrape_bowl.scrape('bowls/phys.pdf', 'PHYS-')
-    scrape_bowl.scrape('bowls/biol.pdf', 'BIOL-')
-    scrape_bowl.scrape('bowls/chem.pdf', 'CHEM-')
+    scrape_bowl.scrape('./data/bowls/phys.pdf', 'PHYS-')
+    scrape_bowl.scrape('./data/bowls/biol.pdf', 'BIOL-')
+    scrape_bowl.scrape('./data/bowls/chem.pdf', 'CHEM-')
 
     breakpoint()
 
